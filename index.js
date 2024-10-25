d2a1979fe7d3f256977839af5765462ff5ef45a3
Abhishek Vibhute <abhishekvibhute2200@gmail.com>
2024-10-25 16:59:11 +0530

    updated project file for development

--- index.js
+++ index.js
@@ -3,50 +3,25 @@ const app = express();
 const port = 8001;
 const mongoose = require('mongoose');
 const ShortUrl = require('./Models/url');
+const urlRoutes = require('./Routes/url')
 
 app.use(express.urlencoded({ extended: false }));
+
+const baseURL = process.env.BASE_URL || 'http://localhost:3000';
 
 // Use the environment variable for MongoDB URI
 const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/urlShortener';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
+
   
 app.set('view engine', 'ejs');
 
-app.get('/', async (req, res) => {
-    const shortUrls = await ShortUrl.find();
-    res.render('index', { shortUrls: shortUrls });
-});
-
-app.post('/shortUrl', async (req, res) => {
-    await ShortUrl.create({ full: req.body.fullUrl }); // Corrected fullUrl name
-    res.redirect('/');
-});
-
-app.get('/:shortUrl', async (req, res) => {
-    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-
-    if (shortUrl == null) return res.status(404).send('URL not found'); // Fixed null check
-
-    shortUrl.clicks++;
-    shortUrl.save();
-
-    res.redirect(shortUrl.full);
-});
-
-app.post('/deleteUrl/:id', async (req,res)=>{
-
-try{
-    await ShortUrl.findByIdAndDelete(req.params.id);
-    res.redirect('/')
-} catch (error) {
-    console.error('Error deleting URL:', error);
-    res.status(500).send('Server Error');
-}
-});
-
+// use the routes fromurlRoutes.js
+app.use('/', urlRoutes);
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
